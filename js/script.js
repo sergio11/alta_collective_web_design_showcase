1a8c07d36d0a6ca93a763e14c090535945ea6b0a
Sergio <sss4esob@gmail.com>
2015-07-16 14:21:13 +0200

    scripts included

---
+++ js/script.js
@@ -0,0 +1,22 @@
+(function($){
+
+	var shrinkHeader = 50;
+
+	var init = function(){
+
+		//Reduction Control Header
+		$(window).scroll(function(){
+			var scroll = window.pageYOffset;
+			if (scroll > shrinkHeader) {
+				$("body").addClass("scrolled");
+			}else{
+				$("body").removeClass("scrolled");
+			}
+		})
+
+	}
+
+	$(document).on("DOMContentLoaded",init);
+
+
+})(jQuery);
